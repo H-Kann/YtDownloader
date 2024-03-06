023322a006b91df5ba43fc74d6ab7744e6ebe5fa
H-Kann <hamzakannouch@gmail.com>
2024-03-06 14:27:06 +0100

    Bug fix

--- YtDownloader_Desktop/info.py
+++ YtDownloader_Desktop/info.py
@@ -1,9 +1,16 @@
 def progressHook(d, downLabel, progressbar):
     # Update label and progress bar with download progress
     if d['status'] == 'downloading':
-        downloaded_percent = int((d["downloaded_bytes"]*100)/d["total_bytes"])
-        downLabel.configure(text=f'Progress: {downloaded_percent}%')
-        progressbar.set(downloaded_percent/100)
+        try:
+            downloaded_percent = int((d["downloaded_bytes"]*100)/d["total_bytes"])
+            print(downloaded_percent)
+            downLabel.configure(text=f'Progress: {downloaded_percent}%')
+            progressbar.set(downloaded_percent/100)
+        except:
+            downloaded_percent = int((d["downloaded_bytes"]*100)/d['total_bytes_estimate'])
+            downLabel.configure(text=f'Progress: {downloaded_percent}%')
+            progressbar.set(downloaded_percent/100)
+
 
 def processingHook(d, downLabel):
     # Show video processing progress
@@ -11,3 +18,4 @@ def processingHook(d, downLabel):
         downLabel.configure(text='Processing Video...')
     elif d['status'] == 'finished':
         downLabel.configure(text='Processing Complete')
+        
