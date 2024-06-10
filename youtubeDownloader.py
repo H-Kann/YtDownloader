ee5ea775d28279ad4d727a13d0b2b27840f443e2
H-Kann <hamzakannouch@gmail.com>
2024-06-10 04:31:46 +0100

    feat: Updated buttons and logo (Experimental)

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -5,15 +5,15 @@ def downloadBestRes(URL, sponsor):
 
     def format_selector(ctx):
         
-            # Sorting format best to worst
+            # Sorting format from worst to best
             formats = ctx.get('formats')[::-1]
             
             # Getting video without audio
             best_video = next(f for f in formats
-                            if f['vcodec'] != 'none' and f['acodec'] == 'none')
+                            if f['vcodec'] != 'none' and f['acodec'] == 'none' and f['ext']== 'mp4')
 
             # Finding compatible audio extension
-            audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
+            audio_ext = {'mp4': 'm4a'}[best_video['ext']]
 
 
             # Geting audio without video
@@ -46,22 +46,30 @@ def downloadBestRes(URL, sponsor):
                         'key': 'ModifyChapters', 
                         'remove_sponsor_segments': ['sponsor', 'intro', 'outro', 'selfpromo', 'preview', 'filler', 'interaction']
                     }
-                ]
+                ],
+                
         }
     else:
-        
+
         ydl_opts = {
             'format': format_selector,
             'outtmpl': "\\Downloads\\%(title)s",
+            'ignoreerrors':True,
+            'extractor_args':{'youtube': {'skip': ['dash']}},
         }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(URL)
 
+
     return True
+
+    
+
 
 
 def downloadWithRes(URL, res, sponsor):
+# Convert 4K to a usable resolution
     
     def checkRes(URL, res):
 
@@ -86,10 +94,16 @@ def downloadWithRes(URL, res, sponsor):
             # Sorting format best to worst
             formats = ctx.get('formats')[::-1]
             
-            # Getting video without audio
-            best_video = next(f for f in formats
-                            if f['vcodec'] != 'none' and f['acodec'] == 'none' and res in f['format'])
-
+            try:
+                # Getting video without audio
+                best_video = None
+                best_video = next(f for f in formats
+                                if f['vcodec'] != 'none' and f['acodec'] == 'none' and res in f['format'] and f['ext']== 'mp4')
+            except:
+                # If resolution + extension combo not found, use next best extension (webm) 
+                if best_video == None:
+                    best_video = next(f for f in formats if f['vcodec'] != 'none' and f['acodec'] == 'none' and res in f['format'])
+                    
             # Finding compatible audio extension
             audio_ext = {'mp4': 'm4a', 'webm': 'webm'}[best_video['ext']]
 
@@ -125,13 +139,17 @@ def downloadWithRes(URL, res, sponsor):
                         'key': 'ModifyChapters', 
                         'remove_sponsor_segments': ['sponsor', 'intro', 'outro', 'selfpromo', 'preview', 'filler', 'interaction']
                     }
-                ]
+                ],
+                'ignoreerrors':True,
+                'extractor_args':{'youtube': {'skip': ['dash']}}
             }
         
         else:
             ydl_opts = {
                 'format': format_selector,
                 'outtmpl': "\\Downloads\\%(title)s",
+                'ignoreerrors':True,
+                'extractor_args':{'youtube': {'skip': ['dash']}}
             }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
